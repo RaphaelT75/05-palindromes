95d65550373e194e2be1d64398946e6c85dc408d
Raphaël Tuet <129511153+RaphaelT75@users.noreply.github.com>
2024-11-14 10:41:05 +0000

    rendu exercice palindromes

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
+"""
+Dans ce module on utlise 2 fonction une secondaire is palindrome qui va vrrifier si
+la chaine de caractères passées en paramètres est un palindrome en tenant compte
+des caractères alphanumériques et une main 
+qui fait des appels et de test de ispalindrome 
+"""
 #### Fonction secondaire
-
-
 def ispalindrome(p):
+    """
+    fonction ispalindrome, permet de déterminer si 
+    un mot ou une chaine des caractères passé en argument est un palindrome
+    renvoie vrai si oui sinon faux
+    """
+    accents = str.maketrans(
+        "áàâäãåçéèêëíìîïñóòôöõúùûüýÿÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝ",
+        "aaaaaaceeeeiiiinooooouuuuyyAAAAAACEEEEIIIINOOOOOUUUUY"
+    )
 
-    # votre code ici
-    
-    return False
+    p = p.translate(accents).lower()
+
+    p = ''.join(char.lower() for char in p if char.isalnum())
+
+    return p == p[::-1]
 
 #### Fonction principale
-
-
 def main():
-
-    # vos appels à la fonction secondaire ici
-
-    for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
+    """
+    fonction main, permet de faire quelques test et appels de la fonction ispalindrome
+    """
+    ispalindrome("test")
+    ispalindrome("anne")
+    ispalindrome("anne anne")
+    for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"] :
         print(s, ispalindrome(s))
 
 
-if __name__ == "__main__":
+if __name__ == "__main__" :
     main()
